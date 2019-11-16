0b98d4b9de0b718ecc9a4b4ea2c6e19945e70b09
tyzo4ka <tyzo4ka@gmail.com>
2019-11-16 11:58:31 +0600

    Added Product Model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('other', 'Other'),
+    ('food', 'Food'),
+    ('clothes', 'Clothes'),
+    ('households', 'Households'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Product")
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0], verbose_name="Category")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Description")
+    picture = models.ImageField(upload_to="product_images", null=True, blank=True)
+
