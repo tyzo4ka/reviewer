a971c41aa626b027ef5ed6153c5b3aed2b9cc46a
tyzo4ka <tyzo4ka@gmail.com>
2019-11-16 13:15:39 +0600

    Registered models in admin, added fixtures

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,6 +9,8 @@ CATEGORY_CHOICES = (
     ('households', 'Households'),
 )
 
+DEFAULT_PRODUCT_IMAGE = "/uploads/products_image/default_image.png"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Product")
@@ -16,10 +18,17 @@ class Product(models.Model):
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Description")
     picture = models.ImageField(upload_to="product_images", null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Review(models.Model):
     author = models.ForeignKey(User, related_name="review", on_delete=models.CASCADE, verbose_name='User')
     product = models.ForeignKey("webapp.Product", related_name='review', on_delete=models.CASCADE, verbose_name="Product")
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Review")
     grade = models.IntegerField(null=False, blank=False, verbose_name="Grade")
+
+    def __str__(self):
+        return f"{self.author} / {self.product}"
+
 
