22f2e671cef7f954e9e325315ef2505aa0341987
tyzo4ka <tyzo4ka@gmail.com>
2019-11-16 14:51:43 +0600

    Added Update View for product

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, ProductDetailView, ProductCreateView
+from .views import IndexView, ProductDetailView, ProductCreateView, ProductUpdateView
 # ProductView, ProductCreateView, BasketChangeView, BasketView,\
 #     ProductUpdateView, ProductDeleteView
 
@@ -9,8 +9,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('products/create/', ProductCreateView.as_view(), name='product_create'),
-    # path('basket/change/', BasketChangeView.as_view(), name='basket_change'),
-    # path('basket/', BasketView.as_view(), name='basket'),
-    # path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     # path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from webapp.forms import ProductForm
 from webapp.models import Product, Review
@@ -24,6 +24,16 @@ class ProductCreateView(CreateView):
     model = Product
     template_name = "products/create_product.html"
     form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse("webapp:product_detail", kwargs={"pk": self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "products/update_product.html"
+    context_object_name = "product"
 
     def get_success_url(self):
         return reverse("webapp:product_detail", kwargs={"pk": self.object.pk})
